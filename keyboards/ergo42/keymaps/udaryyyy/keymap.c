38f8f7c84777d65e7993c15d2b7cf1501c22aa1d
udaryyyy <rinaudaryyy@gmail.com>
2019-06-13 17:16:52 +0900

    右手親指周りのキーマップ変更

--- keyboards/ergo42/keymaps/udaryyyy/keymap.c
+++ keyboards/ergo42/keymaps/udaryyyy/keymap.c
@@ -13,10 +13,10 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE_JP] = LAYOUT( \
-    KC_ESC,   KC_Q,          KC_W,        KC_E,    KC_R,    KC_T,     XXXXXXX,      /*|*/ TO(NUMERIC),  KC_Y,    KC_U,   KC_I,        KC_O,          KC_P,    KC_MINS, \
-    KC_TAB,   KC_A,          KC_S,        KC_D,    KC_F,    KC_G,     KC_LBRC,      /*|*/ KC_RBRC,      KC_H,    KC_J,   KC_K,        KC_L,          KC_SCLN, KC_QUOT, \
-    KC_LSFT,  KC_Z,          KC_X,        KC_C,    KC_V,    KC_B,     LALT(KC_GRV), /*|*/ LALT(KC_GRV), KC_N,    KC_M,   KC_COMM,     KC_DOT,        KC_SLSH, KC_BSLS, \
-    KC_LCTL,  MO(WITH_FUNC), MO(NUMERIC), KC_LALT, KC_DEL,  KC_SPACE, KC_BSPC,      /*|*/ KC_BSPC,     KC_SPACE, KC_ENT, TG(BASE_EN), MO(SHIFT_NUM), XXXXXXX, KC_RGUI \
+    KC_ESC,   KC_Q,          KC_W,        KC_E,    KC_R,    KC_T,     XXXXXXX,      /*|*/ TO(NUMERIC),  KC_Y,     KC_U,     KC_I,   KC_O,        KC_P,         KC_MINS, \
+    KC_TAB,   KC_A,          KC_S,        KC_D,    KC_F,    KC_G,     KC_LBRC,      /*|*/ KC_RBRC,      KC_H,     KC_J,     KC_K,   KC_L,        KC_SCLN,      KC_QUOT, \
+    KC_LSFT,  KC_Z,          KC_X,        KC_C,    KC_V,    KC_B,     LALT(KC_GRV), /*|*/ LALT(KC_GRV), KC_N,     KC_M,     KC_COMM,KC_DOT,      KC_SLSH,      KC_BSLS, \
+    KC_LCTL,  MO(WITH_FUNC), MO(NUMERIC), KC_LALT, KC_DEL,  KC_SPACE, KC_BSPC,      /*|*/ KC_BSPC,      KC_SPACE, KC_SPACE, KC_ENT, TG(BASE_EN), MO(SHIFT_NUM),KC_RGUI \
   ),
 
   [BASE_EN] = LAYOUT( \
