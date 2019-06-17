b46b6549f9e7e02a04b1e6a7f1e65f537e274c1f
udaryyyy <rinaudaryyy@gmail.com>
2019-06-18 00:12:17 +0900

    上矢印を大胆に配置

--- keyboards/ergo42/keymaps/udaryyyy/keymap.c
+++ keyboards/ergo42/keymaps/udaryyyy/keymap.c
@@ -13,14 +13,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT( \
     KC_ESC,   KC_Q,          KC_W,        KC_E,    KC_R,    KC_T,     KC_LBRC,      /*|*/ KC_RBRC,      KC_Y,     KC_U,     KC_I,    KC_O,     KC_P,     KC_MINS, \
     KC_TAB,   KC_A,          KC_S,        KC_D,    KC_F,    KC_G,     KC_LPRN,      /*|*/ KC_RPRN,      KC_H,     KC_J,     KC_K,    KC_L,     KC_SCLN,  KC_QUOT, \
-    KC_LSFT,  KC_Z,          KC_X,        KC_C,    KC_V,    KC_B,     TO(NUMERIC),  /*|*/ LALT(KC_GRV), KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_SLSH,  KC_UP, \
+    KC_LSFT,  KC_Z,          KC_X,        KC_C,    KC_V,    KC_B,     TO(NUMERIC),  /*|*/ LALT(KC_GRV), KC_N,     KC_M,     KC_COMM, KC_DOT,   KC_UP,    KC_SLSH, \
     KC_LCTL,  MO(WITH_FUNC), MO(NUMERIC), KC_LALT, KC_DEL,  KC_SPACE, KC_UNDS,      /*|*/ KC_BSPC,      KC_SPACE, KC_SPACE, KC_ENT,  KC_LEFT,  KC_DOWN,  KC_RIGHT  \
   ),
 
   [NUMERIC] = LAYOUT( \
     KC_ESC,   KC_1,      KC_2,      KC_3,    KC_4,    KC_5,     KC_PSCR,      /*|*/ KC_6,      KC_7,    KC_8,    KC_9,     KC_0,     KC_PMNS, KC_PPLS, \
     KC_TAB,   KC_A,      KC_S,      KC_D,    KC_F,    KC_G,     KC_LBRC,      /*|*/ KC_RBRC,   KC_4,    KC_5,    KC_6,     XXXXXXX,  KC_PSLS, KC_PAST, \
-    KC_LSFT,  KC_Z,      KC_X,      KC_C,    KC_V,    KC_B,     TO(BASE),     /*|*/ KC_DOT,    KC_1,    KC_2,    KC_3,     XXXXXXX,  KC_BSLS, KC_UP,\
+    KC_LSFT,  KC_Z,      KC_X,      KC_C,    KC_V,    KC_B,     TO(BASE),     /*|*/ KC_DOT,    KC_1,    KC_2,    KC_3,     XXXXXXX,  KC_UP,   KC_BSLS, \
     _______,  _______,   _______,   _______, _______, _______,  _______,      /*|*/ _______,   KC_0,    KC_0,    KC_ENT,   KC_LEFT,  KC_DOWN, KC_RIGHT \
   ),
 
