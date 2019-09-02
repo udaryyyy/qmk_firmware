9cbd1d8d4372f2e69df2f2f52f9d89ef6abb9cc0
udaryyyy <rinaudaryyy@gmail.com>
2019-09-02 23:33:00 +0900

    added dvorak-keymap for mint60

---
+++ keyboards/mint60/keymaps/udaryyyy-dvorak/keymap.c
@@ -0,0 +1,116 @@
+/* Copyright 2018 Eucalyn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#ifdef PROTOCOL_LUFA
+#include "lufa.h"
+#include "split_util.h"
+#endif
+
+extern keymap_config_t keymap_config;
+
+#ifdef RGBLIGHT_ENABLE
+//Following line allows macro to read current RGB settings
+extern rgblight_config_t rgblight_config;
+#endif
+
+enum custom_keycodes {
+  RGBRST = SAFE_RANGE
+};
+
+enum layers {
+  BASE = 0,
+  WITH_FN_KEY,
+  NUMERIC,
+  DEFAULT
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [BASE] = LAYOUT( \
+    KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,      KC_7,    KC_8,    KC_9,    KC_0,   KC_LBRC, KC_RBRC, KC_BSPC,\
+    KC_TAB,    KC_QUOT,  KC_COMM, KC_DOT, KC_P,    KC_Y,           KC_F,    KC_G,    KC_C,      KC_R,    KC_L,   KC_SLSH, KC_EQL,KC_BSLS \
+    LALT(KC_GRV),     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,      KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,        KC_ENT,   \
+    KC_LSFT,       KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X,      KC_B,    KC_M,     KC_W,   KC_V,  KC_Z  TO(NUMERIC), KC_UP,TO(DEFAULT),  \
+    KC_LCTL,   KC_LALT,    KC_LGUI, MO(WITH_FN_KEY),  KC_SPC,     KC_DEL,  KC_BSPC, LALT(KC_GRV),               KC_LEFT,KC_DOWN,KC_RGHT \
+  ),
+  [WITH_FN_KEY] = LAYOUT( \
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, LALT(KC_GRV), _______, \
+    RGB_TOG,   RGBRST,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    KC_PSCR,     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        _______, \
+    _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_PGUP, LALT(KC_DEL), \
+    _______,   _______,    _______,    _______,    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_END \
+  ),
+  [NUMERIC] = LAYOUT( \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PMNS, KC_PPLS, LALT(KC_GRV), KC_BSPC, \
+    KC_TAB,    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, KC_7,    KC_8,    KC_9,    KC_PSLS, KC_PAST,  XXXXXXX,    XXXXXXX, \
+    KC_LCTL,     KC_A,    KC_S,    XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX,  KC_ENT, \
+    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,   XXXXXXX,   KC_PDOT, KC_1,    KC_2,    KC_3,    XXXXXXX,  _______, KC_UP,  KC_ESC, \
+    KC_LCTL,   KC_LALT,    KC_LGUI,    _______,    KC_SPC,       KC_DEL,  KC_BSPC,  KC_0,                      KC_LEFT, KC_DOWN, KC_RGHT \
+  ),
+  [DEFAULT] = LAYOUT( \
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSPC,  \
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
+    KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
+    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TG(NUMERIC),   KC_UP,  KC_ESC,   \
+    KC_LCTL,   KC_LALT,    KC_LGUI, MO(WITH_FN_KEY),  KC_SPC,     KC_DEL,  KC_BSPC, LALT(KC_GRV),               KC_LEFT,KC_DOWN,KC_RGHT \
+  )
+};
+
+// define variables for reactive RGB
+bool TOG_STATUS = false;
+int RGB_current_mode;
+
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+          if (record->event.pressed) {
+            register_code(KC_RSFT);
+          } else {
+            unregister_code(KC_RSFT);
+          }
+        break;
+      }
+    return MACRO_NONE;
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RGBRST:
+      #ifdef RGBLIGHT_ENABLE
+        if (record->event.pressed) {
+          eeconfig_update_rgblight_default();
+          rgblight_enable();
+          RGB_current_mode = rgblight_config.mode;
+        }
+      #endif
+      break;
+  }
+  return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
