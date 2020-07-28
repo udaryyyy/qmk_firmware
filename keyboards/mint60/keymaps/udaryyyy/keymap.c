4e250c6f9dd8091982c4baaff55874e0172a82bf
udaryyyy <rinaudaryyy@gmail.com>
2020-07-28 18:15:27 +0900

    keymap changed

--- keyboards/mint60/keymaps/udaryyyy/keymap.c
+++ keyboards/mint60/keymaps/udaryyyy/keymap.c
@@ -38,24 +38,24 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT( \
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSPC,  \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  \
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
-    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TG(NUMERIC),   KC_UP,  KC_ESC,   \
-    KC_LCTL,   KC_LALT,    KC_LGUI, MO(WITH_FN_KEY),  KC_SPC,     KC_DEL,  KC_BSPC, LALT(KC_GRV),               KC_LEFT,KC_DOWN,KC_RGHT \
+    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,KC_UP, TG(NUMERIC),   \
+    KC_LCTL,   KC_LALT,    KC_LGUI, MO(WITH_FN_KEY),  KC_SPC,     KC_BSPC,  KC_ENT, LALT(KC_GRV),               KC_LEFT,KC_DOWN,KC_RGHT \
   ),
   [WITH_FN_KEY] = LAYOUT( \
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, LALT(KC_GRV), _______, \
-    RGB_TOG,   RGBRST,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    KC_PSCR,     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        _______, \
-    _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_PGUP, LALT(KC_DEL), \
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, LALT(KC_GRV), KC_DEL, \
+    RGB_TOG,   RGBRST,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX, \
+    KC_PSCR,     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_RGHT,        _______, \
+    _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DOWN, _______, KC_PGUP, _______, \
     _______,   _______,    _______,    _______,    XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_END \
   ),
   [NUMERIC] = LAYOUT( \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PMNS, KC_PPLS, LALT(KC_GRV), KC_BSPC, \
     KC_TAB,    XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, KC_7,    KC_8,    KC_9,    KC_PSLS, KC_PAST,  XXXXXXX,    XXXXXXX, \
     KC_LCTL,     KC_A,    KC_S,    XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, XXXXXXX,  KC_ENT, \
-    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,   XXXXXXX,   KC_PDOT, KC_1,    KC_2,    KC_3,    XXXXXXX,  _______, KC_UP,  KC_ESC, \
+    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,   XXXXXXX,   KC_PDOT, KC_1,    KC_2,    KC_3,    XXXXXXX,  _______, KC_UP,  _______, \
     KC_LCTL,   KC_LALT,    KC_LGUI,    _______,    KC_SPC,       KC_DEL,  KC_BSPC,  KC_0,                      KC_LEFT, KC_DOWN, KC_RGHT \
   )
 };
