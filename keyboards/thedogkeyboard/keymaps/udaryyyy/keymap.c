cd52b0202d703b0afe6d42bd07ba8e62088ccaf2
udaryyyy <rinaudaryyy@gmail.com>
2020-08-14 00:10:21 +0900

    RGBLIGHT_ENABLE

--- keyboards/thedogkeyboard/keymaps/udaryyyy/keymap.c
+++ keyboards/thedogkeyboard/keymaps/udaryyyy/keymap.c
@@ -23,6 +23,15 @@ enum layers
 
 #define KC_COMMAND LT(COMMAND, KC_F13)
 
+#ifdef RGBLIGHT_ENABLE
+//Following line allows macro to read current RGB settings
+extern rgblight_config_t rgblight_config;
+#endif
+
+enum custom_keycodes {
+  RGBRST = SAFE_RANGE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_all( /* BASE */
         KC_ESC,   KC_F1,KC_F2,KC_F3,KC_F4,  KC_F5,KC_F6,KC_F7,KC_F8,  KC_F9,KC_F10,KC_F11,KC_F12,  KC_PSCREEN,KC_SCROLLLOCK,KC_PAUSE,
@@ -36,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_F1,KC_F2,KC_F3,KC_F4,  KC_F5,KC_F6,KC_F7,KC_F8,  KC_F9,KC_F10,KC_F11,KC_F12,  KC_PSCREEN,KC_SCROLLLOCK,KC_PAUSE,
 
         KC_GRV,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINS,KC_EQL,KC_BSPC,        KC_INS, KC_HOME, KC_PGUP,         KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-        KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_Y,KC_U,KC_UP,KC_O,KC_P,KC_LBRC,KC_RBRC,KC_BSLS,       KC_DEL,  KC_END, KC_PGDN,           KC_P7,   KC_P8,   KC_P9, KC_PPLS,
-        KC_LCAP, KC_A,KC_S,KC_D,KC_F,KC_G,KC_H,KC_LEFT,KC_RIGHT,KC_L,KC_SCLN,KC_QUOT    ,KC_ENT,                                           KC_P4,   KC_P5,   KC_P6,   KC_NO,
+        RGB_TOG,RGBRST,RGB_HUI, RGB_SAI, RGB_VAI,KC_T,KC_Y,KC_U,KC_UP,KC_O,KC_P,KC_LBRC,KC_RBRC,KC_BSLS,       KC_DEL,  KC_END, KC_PGDN,           KC_P7,   KC_P8,   KC_P9, KC_PPLS,
+        KC_LCAP, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,KC_G,KC_H,KC_LEFT,KC_RIGHT,KC_L,KC_SCLN,KC_QUOT    ,KC_ENT,                                           KC_P4,   KC_P5,   KC_P6,   KC_NO,
         KC_LSHIFT, KC_Z,KC_X,KC_C,KC_V,KC_B,KC_N,KC_DOWN,KC_COMM,KC_DOT,KC_SLSH,  KC_RSHIFT,                 KC_UP,                    KC_P1,   KC_P2,   KC_P3, KC_PENT,
         KC_LCTL,KC_LALT,_______,KC_BSPC,KC_SPC,KC_ENT,KC_SPC,KC_BSPC,LALT(KC_GRV),KC_NO,KC_NO,_______,   KC_LEFT,KC_DOWN,KC_RIGHT,           KC_P0,   KC_NO, KC_PDOT,   KC_NO ),
 };
